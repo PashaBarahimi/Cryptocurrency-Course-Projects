9ff37c3e8f06ec153992367f93fb2fbf1199e3b9
Pasha Barahimi <pashabarahimi@gmail.com>
2023-05-19 20:57:58 +0330

    Add Transaction class

--- CA1/codes/transaction.py
+++ CA1/codes/transaction.py
@@ -1,60 +1,171 @@
+import enum
+
+import base58
 import bitcoin.wallet
-from bitcoin.core import COIN, b2lx, serialize, x, lx, b2x
-from utils import *
+import requests
+from bitcoin.core import (
+    COIN,
+    CMutableTransaction,
+    CMutableTxIn,
+    CMutableTxOut,
+    COutPoint,
+    Hash160,
+    b2x,
+    lx,
+)
+from bitcoin.core.script import *
+from bitcoin.core.scripteval import SCRIPT_VERIFY_P2SH, VerifyScript
 
-bitcoin.SelectParams("testnet")  # Select the network (testnet or mainnet)
-# Private key in WIF format XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-my_private_key = bitcoin.wallet.CBitcoinSecret("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-my_public_key = my_private_key.pub
-my_address = bitcoin.wallet.P2PKHBitcoinAddress.from_pubkey(my_public_key)
-destination_address = bitcoin.wallet.CBitcoinAddress('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')  # Destination address (recipient of the money)
-
-
-def P2PKH_scriptPubKey(address):
-    ######################################################################
-    ## Fill out the operations for P2PKH scriptPubKey                   ##
-
-    return [..., OP_CHECKSIG]  # Fill this section
-    ######################################################################
+TRANSACTION_BROADCAST_URL = "https://api.blockcypher.com/v1/btc/test3/txs/push"
 
 
-def P2PKH_scriptSig(txin, txout, txin_scriptPubKey):
-    ######################################################################
-    ## Fill out the operations for P2PKH scriptSig                      ##
-
-    signature = create_OP_CHECKSIG_signature(txin, txout, txin_scriptPubKey, my_private_key)
-
-    return [signature, ...]  # Fill this section
-    ######################################################################
+def address_to_pub_key_hash160(address: str) -> bytes:
+    pub_key_hash = base58.b58decode_check(address)[1:]
+    return pub_key_hash
 
 
-def send_from_P2PKH_transaction(amount_to_send, txid_to_spend, utxo_index, txout_scriptPubKey):
-    txout = create_txout(amount_to_send, txout_scriptPubKey)
-
-    txin_scriptPubKey = P2PKH_scriptPubKey(my_address)
-    txin = create_txin(txid_to_spend, utxo_index)
-    txin_scriptSig = P2PKH_scriptSig(txin, txout, txin_scriptPubKey)
-
-    new_tx = create_signed_transaction(txin, txout, txin_scriptPubKey, txin_scriptSig)
-
-    return broadcast_transaction(new_tx)
+def P2PKH_script_pub_key(pub_key_hash: bytes) -> CScript:
+    return CScript([OP_DUP, OP_HASH160, pub_key_hash, OP_EQUALVERIFY, OP_CHECKSIG])  # type: ignore
 
 
-def main():
-    ######################################################################
-    amount_to_send = 0.1
-    txid_to_spend = ('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')  # TxHash of UTXO
-    utxo_index = 0  # UTXO index among transaction outputs
-    ######################################################################
+class Destination:
+    def __init__(
+        self, address: str, amount: float, script_pub_key: CScript | None = None
+    ):
+        self._address = address
+        self._script_pub_key = (
+            P2PKH_script_pub_key(address_to_pub_key_hash160(address))
+            if script_pub_key is None
+            else script_pub_key
+        )
+        self._amount = amount
 
-    print(my_address)  # Prints your address in base58
-    print(my_public_key.hex())  # Print your public key in hex
-    print(my_private_key.hex())  # Print your private key in hex
-    txout_scriptPubKey = P2PKH_scriptPubKey(my_address)
-    response = send_from_P2PKH_transaction(amount_to_send, txid_to_spend, utxo_index, txout_scriptPubKey)
-    print(response.status_code, response.reason)
-    print(response.text)  # Report the hash of transaction which is printed in this section result
+    @property
+    def address(self) -> str:
+        return self._address
+
+    @property
+    def script_pub_key(self) -> CScript:
+        return self._script_pub_key
+
+    @property
+    def amount(self) -> float:
+        return self._amount
+
+    @property
+    def TxOut(self) -> CMutableTxOut:
+        return CMutableTxOut(int(self.amount * COIN), self.script_pub_key)
 
 
-if __name__ == '__main__':
-    main()
+class UnspentTransactionOutput:
+    def __init__(
+        self,
+        tx_id: str,
+        index: int,
+        script_pub_key: CScript,
+        custom_sig: CScript | None = None,
+    ):
+        self._tx_id = tx_id
+        self._index = index
+        self._script_pub_key = script_pub_key
+        self._custom_sig = custom_sig
+
+    @property
+    def tx_id(self) -> str:
+        return self._tx_id
+
+    @property
+    def index(self) -> int:
+        return self._index
+
+    @property
+    def script_pub_key(self) -> CScript:
+        return self._script_pub_key
+
+    @property
+    def custom_sig(self) -> CScript | None:
+        return self._custom_sig
+
+    @property
+    def TxIn(self) -> CMutableTxIn:
+        return CMutableTxIn(COutPoint(lx(self.tx_id), self.index))
+
+
+class Transaction:
+    class Network(enum.Enum):
+        MAINNET = "mainnet"
+        TESTNET = "testnet"
+
+    def __init__(self, private_key: str, network: Network = Network.TESTNET):
+        self._network = network
+        bitcoin.SelectParams(self._network.value)
+
+        self._private_key = bitcoin.wallet.CBitcoinSecret(private_key)
+        self._public_key = self._private_key.pub
+        self._address = bitcoin.wallet.P2PKHBitcoinAddress.from_pubkey(self._public_key)
+        self._destinations = []
+        self._utxos = []
+        self._tx = CMutableTransaction()
+
+    @property
+    def address(self) -> str:
+        return str(self._address)
+
+    def add_destination(self, destinations: Destination) -> None:
+        self._destinations.append(destinations)
+
+    def add_utxo(self, utxos: UnspentTransactionOutput) -> None:
+        self._utxos.append(utxos)
+
+    def create(self) -> requests.Response:
+        if not self._destinations:
+            raise ValueError("No destinations were added to the transaction")
+        if not self._utxos:
+            raise ValueError(
+                "No unspent transaction outputs were added to the transaction"
+            )
+        self._create_transaction()
+        self._verify()
+        return self._broadcast_transaction()
+
+    def my_P2PKH_script_pub_key(self) -> CScript:
+        return P2PKH_script_pub_key(Hash160(self._public_key))
+
+    def _my_P2PKH_script_sig(self, txin_script_pub_key: CScript) -> CScript:
+        signature = self._create_OP_CHECKSIG_signature(txin_script_pub_key)
+        return CScript([signature, self._public_key])  # type: ignore
+
+    def _create_transaction(self) -> None:
+        txins = [utxo.TxIn for utxo in self._utxos]
+        txouts = [destination.TxOut for destination in self._destinations]
+        self._tx = CMutableTransaction(txins, txouts)
+
+    def _create_OP_CHECKSIG_signature(self, txin_script_pub_key: CScript) -> bytes:
+        sighash = SignatureHash(txin_script_pub_key, self._tx, 0, SIGHASH_ALL)
+        sig = self._private_key.sign(sighash) + bytes([SIGHASH_ALL])  # type: ignore
+        return sig
+
+    def _verify(self):
+        for i, _ in enumerate(self._utxos):
+            txin_script_pub_key = self._utxos[i].script_pub_key
+            txin_script_sig = self._utxos[i].custom_sig
+            if txin_script_sig is None:
+                txin_script_sig = self._my_P2PKH_script_sig(txin_script_pub_key)
+            self._tx.vin[i].scriptSig = txin_script_sig
+            VerifyScript(
+                self._tx.vin[i].scriptSig,
+                txin_script_pub_key,
+                self._tx,
+                i,
+                (SCRIPT_VERIFY_P2SH,),
+            )
+
+    def _broadcast_transaction(self) -> requests.Response:
+        raw_transaction = b2x(self._tx.serialize())
+        headers = {"content-type": "application/x-www-form-urlencoded"}
+        return requests.post(
+            TRANSACTION_BROADCAST_URL,
+            headers=headers,
+            data='{"tx": "%s"}' % raw_transaction,
+            timeout=60,
+        )
